93d6d8f7ab498b5b52d87dfc50cc861aef8499cf
Shaurave <kuwarsaurav21@gmail.com>
2024-12-03 20:19:00 +0100

    reformated

--- spark/src/pyspark/spark_elastic_data_load_demo.py
+++ spark/src/pyspark/spark_elastic_data_load_demo.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import types as T
 
+# Initialize spark session
 spark = (
     SparkSession
         .builder
@@ -11,24 +12,24 @@ spark = (
         .appName("SparkToElastic")
         .getOrCreate()
 )
+# Set log level
 spark.sparkContext.setLogLevel("ERROR")
 
+# Schema of data
 schema = T.StructType([
     T.StructField("name", T.StringType(), True),
     T.StructField("age", T.IntegerType(), True)
 ])
 
+# Sample data
 data = [
-    ["Shaurave", 36],
-    ["Samjhana", 33],
+    ["John", 36],
+    ["Doe", 33],
 ]
-
+# Create Spark DataFrame
 df = spark.createDataFrame(data, schema)
 
-df.show(truncate=False)
-
 # Write DataFrame to Elasticsearch
-
 (
     df.write
         .format("org.elasticsearch.spark.sql")
